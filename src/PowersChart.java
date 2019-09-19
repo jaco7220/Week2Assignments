6a568f86161e17a97ae514786930e215d060bd62
jaco7220 <jaco7220@ISS2V1SKH2.scdsb.pri>
2019-09-19 11:35:03 -0400

    9/19/19

--- src/PowersChart.java
+++ src/PowersChart.java
@@ -1,16 +1,36 @@
 
 import java.util.Scanner;
 
-
 public class PowersChart {
 
-    
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
-        while(true){
-            System.out.println("");
+        int expo;
+        while (true) {
+            System.out.print("Enter an exponent value: ");
+            if (s.hasNextInt()) {
+                expo = s.nextInt();
+            } else {
+                System.out.println("Invalid, try again");
+                s.nextLine();
+                continue;
+            }
+            System.out.println("past contrine");
+            if (expo >= 0 && expo <= 10) {
+                break;
+            } else {
+                System.out.println("Invalid, try again");
+            }
         }
+
+        System.out.format("X");
+        System.out.format("\t  2^X\n");
+        System.out.println("-------------");
+        for (int i = 0; i <= expo; i++) {
+            System.out.format("%d", i);
+            System.out.format("\t2^%d=%d\n",i,(int)Math.pow(2, i));
+        }
+
     }
-    
+
 }
--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -3,7 +3,29 @@ public class SalaryTable {
 
     
     public static void main(String[] args) {
+        double oldsal=40000, newsal=40000;
+        System.out.format("%s","YEAR");
+        System.out.format("%15s","OLD SALARY");
+        System.out.format("%10s","RAISE");
+        System.out.format("%15s","NEW SALARY\n");
+        System.out.format("%s","----");
+        System.out.format("%15s","----------");
+        System.out.format("%10s","-----");
+        System.out.format("%15s","----------\n");
         
+        System.out.format("%2d",1);
+            System.out.format("%14s","---");
+            System.out.format("%16s","$40000.00");
+            System.out.format("%11s\n","$40000.00");
+            
+        for (int i = 1; i < 10; i++) {
+            oldsal=newsal;
+            newsal*=1.03;
+            System.out.format("%2d",i+1);
+            System.out.format("%9s%.2f","$",oldsal);
+            System.out.format("%5s%.2f","$",oldsal*0.03);
+            System.out.format("%4s%.2f\n","$",newsal);
+        }
     }
     
 }
